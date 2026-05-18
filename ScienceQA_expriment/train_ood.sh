40d1452cc2bf9e23d9f3930cf7885a64090f25a6
Yejun Oh <oyjgod02@korea.ac.kr>
2026-05-18 21:42:16 +0900

    Revert "생물학만 v.s. 4종을 모두 망각하는 걸 주석으로 바꿀 수 있도록 개선"
    This reverts commit d6cfb291888d64b4a6c7c8b0e8b0b0a0bbef03c2.

--- ScienceQA_expriment/train_ood.sh
+++ ScienceQA_expriment/train_ood.sh
@@ -1,21 +1,15 @@
 for SEED in 0 1 2
 do
   TYPE=""
-  # for UNLEAN_D in "biology" "physics" "chemistry" "economics"
-  ## revised: 생물학만 v.s. 4종을 모두 망각하는 걸 주석으로 바꿀 수 있도록 개선
-  DATASETS=("biology") # 생물학만을 망각시킬 경우
-  # DATASETS=("biology" "physics" "chemistry" "economics") # 4종을 모두 망각시킬 경우
-  
-  for UNLEAN_D in "${DATASETS[@]}"
+  for UNLEAN_D in "biology" "physics" "chemistry"
   do
       TYPE+="_${UNLEAN_D}"
       OODPATH_1="./data/scienceqa_RD_5/scienceqa_not${TYPE}"
-        
       python train_ood.py \
-          --unlearn_dataset "scienceqa_${UNLEAN_D}" \
-          --ood_dataset "ood_scienceqa" \
-          --base_unlearn_path "./data/scienceqa/" \
-          --base_ood_path ${OODPATH_1} \
-          --seed ${SEED}
+           --unlearn_dataset "scienceqa_${UNLEAN_D}" \
+           --ood_dataset "ood_scienceqa" \
+           --base_unlearn_path "./data/scienceqa/" \
+           --base_ood_path ${OODPATH_1} \
+           --seed ${SEED}
   done
 done
